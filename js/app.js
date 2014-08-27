d82b9072708acac481b35affa96082b8714019cd
Juan Posadas <jposadas@stanford.edu>
2014-08-27 18:29:36 +0200

    complete reorganization

--- js/app.js
+++ js/app.js
@@ -7,53 +7,53 @@ app.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider,
 	$stateProvider
 		.state('questionnaire', {
 			url: '/questionnaire',
-			templateUrl: 'questionnaire.html',
+			templateUrl: 'templates/questionnaire.html',
 			controller: 'questionnaireCtrl'
 		})
 		.state('questionnaire.question1', {
 			url: '/question1',
-			templateUrl: 'questionnaire.question1.html',
+			templateUrl: 'templates/questionnaire.question1.html',
 			controller: 'questionCtrl'
 		})
 		.state('questionnaire.question2', {
 			url: '/question2',
-			templateUrl: 'questionnaire.question2.html',
+			templateUrl: 'templates/questionnaire.question2.html',
 			controller: 'questionCtrl'
 		})
 		.state('questionnaire.question3', {
 			url: '/question3',
-			templateUrl: 'questionnaire.question3.html',
+			templateUrl: 'templates/questionnaire.question3.html',
 			controller: 'questionCtrl'
 		})
 		.state('questionnaire.submit', {
 			url: '/submit',
-			templateUrl: 'questionnaire.submit.html',
+			templateUrl: 'templates/questionnaire.submit.html',
 			controller: 'submitCtrl'
 		})
 		.state('registration', {
 			url: '/registration',
-			templateUrl: 'registration.html',
+			templateUrl: 'templates/registration.html',
 			controller: 'registrationCtrl'
 		})
 		.state('registration.reg1', {
 			url: '/reg1',
-			templateUrl: 'registration.reg1.html'
+			templateUrl: 'templates/registration.reg1.html'
 		})
 		.state('registration.reg2', {
 			url: '/reg2',
-			templateUrl: 'registration.reg2.html'
+			templateUrl: 'templates/registration.reg2.html'
 		})
 		.state('banner', {
 			url: '/banner',
-			templateUrl: 'banner.html'
+			templateUrl: 'templates/banner.html'
 		})
 		.state('banner.list', {
 			url: '/list',
-			templateUrl: 'banner.list.html'
+			templateUrl: 'templates/banner.list.html'
 		})
 		.state('banner.outfit', {
 			url: '/outfit/:id',
-			templateUrl: 'banner.outfit.html'
+			templateUrl: 'templates/banner.outfit.html'
 		});
 
 }]);
